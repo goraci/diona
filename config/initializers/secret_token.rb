5905dc78991a2aa5e421348195366116c934c161
Roman <felixcon@gmail.com>
2011-09-01 20:49:15 +0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diona::Application.config.secret_token = '2f6887c1af839a076b6c3356cb8976dcae3459d5221a5cb29befd7ad2c760f487dbe9e63e5e1bc4045379536f83d322900484a0eaf5f154bf23c3ed4d8bfbcb0'
